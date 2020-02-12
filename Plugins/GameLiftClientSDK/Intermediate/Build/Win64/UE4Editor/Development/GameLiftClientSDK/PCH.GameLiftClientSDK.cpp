a6a939f23079c3e7bc6b644a96a82e908c4810cb
Chase Wex <clapingmanly@hotmail.com>
2020-02-11 22:17:13 -0500

    cleaned file so we only have what we need

--- Plugins/GameLiftClientSDK/Intermediate/Build/Win64/UE4Editor/Development/GameLiftClientSDK/PCH.GameLiftClientSDK.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Chase/Documents/Unreal Projects/ProjectCougar/Plugins/GameLiftClientSDK/Intermediate/Build/Win64/UE4Editor/Development/GameLiftClientSDK/PCH.GameLiftClientSDK.h"
--- Plugins/GameLiftServerSDK/Intermediate/Build/Win64/UE4Editor/Development/GameLiftServerSDK/PCH.GameLiftServerSDK.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Chase/Documents/Unreal Projects/ProjectCougar/Plugins/GameLiftServerSDK/Intermediate/Build/Win64/UE4Editor/Development/GameLiftServerSDK/PCH.GameLiftServerSDK.h"
